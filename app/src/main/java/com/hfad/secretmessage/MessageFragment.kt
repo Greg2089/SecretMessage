cbe6454d23709050056e3036602a68a7d02f3697
Grigoriy Lyalko <98054846+Greg2089@users.noreply.github.com>
2023-03-14 11:27:59 +0300

    Создал EncryptFragment и action для перехода на него из MessageFragment

--- app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -5,7 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import androidx.navigation.findNavController
 
 
 class MessageFragment : Fragment() {
@@ -14,6 +15,13 @@ class MessageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val buttonNext = view.findViewById<Button>(R.id.next)
+        buttonNext.setOnClickListener {
+            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
+
+        return view
     }
 }
+
